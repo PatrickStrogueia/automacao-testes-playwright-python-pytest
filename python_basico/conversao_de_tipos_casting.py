d7c197a1a7ca5a74402b65d89dea68764f281fb7
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-23 21:37:11 -0400

    Aula 11

---
+++ python_basico/conversao_de_tipos_casting.py
@@ -0,0 +1,27 @@
+valor1 = input("Digite o primeiro número: ")
+valor2 = input("Digite o segundo número: ")
+
+print(type(valor1))
+print(type(valor2))
+
+valor1 = int(valor1)
+valor2 = int(valor2)
+
+print(type(valor1))
+print(type(valor2))
+
+print(valor1 + valor2)
+
+valor_decimal = input("Digite um valor decimal: ")
+valor_decimal = float(valor_decimal)
+print(type(valor_decimal))
+print(valor_decimal)
+
+total = 150
+mensagem = "Valor total: " + str(total) # Se o valor de total não for convertido o sistema apresenta erro
+print(mensagem)
+
+valor1 = int(input("Digite o primeiro número: "))
+valor2 = int(input("Digite o segundo número: "))
+resultado = valor1 + valor2
+print(resultado)
