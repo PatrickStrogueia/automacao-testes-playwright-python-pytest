d408c47a7ebb64226f1fef5395e872902362ea8f
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-23 21:53:43 -0400

    Aula 12

---
+++ python_basico/estruturas_condicionais_if_elif_else.py
@@ -0,0 +1,31 @@
+idade = int(input("Informe a sua idade: "))
+
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
+nota = float(input("Informe sua nota escolar: "))
+
+# ---
+
+if nota >= 9:
+    print("Excelente!")
+elif nota >= 7:
+    print("Bom.")
+else:
+    print("Precisa melhorar.")
+
+# ---
+
+idade = 20
+
+if idade > 18:
+    print("Maior que 18.")
+
+# ---
+
+idade = 18
+
+if idade == 18:
+    print("Igual a 18.")
