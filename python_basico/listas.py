3b284baf08afc979d7c6ad5e3554aee94eb3f5b1
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-08-04 23:00:20 -0400

    Aula 14 - parcial

---
+++ python_basico/listas.py
@@ -0,0 +1,26 @@
+ambientes = ["desenvolvimento", "homologação", "produção"]
+
+print(ambientes[0])
+print(ambientes[1])
+print(ambientes[2])
+
+# exibição da lista em ordem inversa
+print(ambientes[-1])
+print(ambientes[-2])
+print(ambientes[-3])
+
+# exemplos para QAs
+
+execucoes = ["PASSOU", "PASSOU", "FALHOU", "PASSOU"]
+
+ultimo_resultado = execucoes[-1]
+print(f"Resultado mais recente: {ultimo_resultado}")
+
+# modificando valores
+
+usuarios = ["ana@email.com", "carlso@email.com", "maria@email.com"]
+
+usuarios[1] = "carlos@email.com"
+
+print(usuarios)
+
